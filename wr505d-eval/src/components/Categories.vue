390e3a8bf6cff874145036a175b396027bac986d
TayeYanis <yanis.taye.2001@gmail.com>
2024-10-05 17:24:34 +0200

    feat: search

--- wr505d-eval/src/components/Categories.vue
+++ wr505d-eval/src/components/Categories.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="categories-container">
     <h1>Categories</h1>
-    <ul v-if="categories.length > 0">
+    <!-- CHAMP DE REHCHERCHE VIA INPUT APPLIQUATION D'UN PLACEHOLDER POUR INDIQUER CE QUI EST MIT PAR DEFAULT -->
+    <input
+        type="text"
+        v-model="searchQuery"
+        placeholder="Rechercher une catégorie..."
+        class="search-bar"
+    />
+
+    <ul v-if="filteredCategories.length > 0">
       <p>Liste des catégories</p>
-      <li v-for="category in categories" :key="category.id">
+      <!-- MISE EN PLACE D'UN BOUCLE POUR LES CATEGORIES TROUVER PAR LA RECHERCHE -->
+      <li v-for="category in filteredCategories" :key="category.id">
         {{ category.title }}
       </li>
     </ul>
@@ -19,8 +28,17 @@ export default {
   data() {
     return {
       categories: [],  // STOCKAGE DE category sur un tableau
-      error: null      // gestion en cas d'erreur
+      error: null,      // gestion en cas d'erreur
+      searchQuery: '',  // Analyse du texte entrée par l'utilisateur
     };
+  },
+  computed: {
+    // FILTRAGE DES CATEGORIES CELON LES CONTENUE DU CHAMP DE RECHERCE
+    filteredCategories() {
+      return this.categories.filter(category =>
+          category.title.toLowerCase().includes(this.searchQuery.toLowerCase())
+      );
+    }
   },
   created() {
     this.fetchCategories();  // Appeler la méthode afin de récuperer les élement de l'entiter Catégorie
@@ -46,6 +64,16 @@ export default {
 
   h1 {
     margin-bottom: 20px;
+  }
+
+  .search-bar {
+    margin-bottom: 20px;
+    padding: 10px;
+    width: 100%;
+    max-width: 400px;
+    font-size: 16px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
   }
 
   ul {
